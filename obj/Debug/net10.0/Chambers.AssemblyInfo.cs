db978175a439d92ed9b9bc22d05b752eb8781b5a
cybutr <simekadam007@gmail.com>
2026-04-30 21:21:56 +0200

    assembly-info

--- obj/Debug/net10.0/Chambers.AssemblyInfo.cs
+++ obj/Debug/net10.0/Chambers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chambers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9a2bc5bb26cfa78342775ecf23402020cd02ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d937fc979b7427a5e86c1f8584ab9499774e108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chambers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chambers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
